5c92d987c7d4b2b24db01171cce6705ba7ef9963
ArtjomsVasiljevs <artjoms.vasiljevs@inbox.lv>
2023-02-18 23:18:20 +0200

    try 1

--- main.py
+++ main.py
@@ -11,21 +11,43 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    count = -1
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next,i))
-            
             pass
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+             if(len(opening_brackets_stack)==0 or not opening_brackets_stack[-1].are_matching(next)):
+                count = i +1
+        if(count == -1 and len(opening_brackets_stack)==0):
+            return 'Success'
+        else:
+            return count
+            
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    inputW = input("F or I")
+
+    if inputW == "F":
+        file_path = input("file path:")
+        with open("input.txt", "r") as f:
+            text = f.read()
+            mismatch = find_mismatch(text)
+            if mismatch == 'Success':
+                print("Success")
+            else:
+                print(mismatch)
+
+    elif inputW == "I":
+        text = input()
+        mismatch = find_mismatch(text)
+        if mismatch == 'Success':
+            print("Success")
+        else:
+            print(mismatch)
+    
 
 
 if __name__ == "__main__":
